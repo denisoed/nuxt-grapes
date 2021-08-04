d52ae2b15ed69a1845f5a81b5e0ea482e576617f
Denisoed <denisoed@gmail.com>
2021-08-04 20:09:25 +0600

    Init proj

---
+++ pages/admin.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="gjs"></div>
+</template>
+
+<script>
+export default {
+  name: 'Admin',
+  mounted() {
+    this.$grapes({
+      container: "#gjs"
+    });
+  },
+}
+</script>
---
+++ plugins/grapes.js
@@ -0,0 +1,16 @@
+import grapes from 'grapesjs';
+import 'grapesjs-blocks-basic';
+import 'grapesjs/dist/css/grapes.min.css';
+
+const defaultOptions = {
+  plugins: ["gjs-blocks-basic"]
+};
+
+export default ({ app }, inject) => { // eslint-disable-line
+  inject('grapes', (options) => {
+    grapes.init({
+      ...defaultOptions,
+      ...options,
+    });
+  });
+};
